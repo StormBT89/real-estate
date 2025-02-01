d5325a3999bdf9738beeae404f27f0b3d00c54ae
StormBT89 <glavevski.pece@gmail.com>
2025-02-01 02:16:36 +0100

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useRef, useState } from "react";
 import { set } from "mongoose";
-import {updateUserStart, updateUserSuccess, updateUserFailure} from '../redux/user/userSlice.js';
+import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice.js';
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -45,6 +45,27 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+
+
+  }
   return (
     <div className="p-3 max-w-lg mx-auto"> 
       <h1 className='text-3xl font-semibold text-center my-7'>Кориснички профил</h1>
@@ -61,7 +82,7 @@ export default function Profile() {
           </button>       
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Избришете го корисничкиот профил</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Избришете го корисничкиот профил</span>
         <span className="text-red-700 cursor-pointer">Одјавете се</span>
       </div>
     <p className="text-red-700 mt-5">{error ? error : ''}</p>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
-import { updateUser } from '../../../../api/controllers/user.controller';
+import { deleteUser, updateUser } from '../../../../api/controllers/user.controller';
 
 const initialState = {
     currentUser: null,
@@ -35,10 +35,23 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
     },
 });
 
 export const {signInStart, signInSuccess, singInFailure,
-    updateUserStart, updateUserSuccess, updateUserFailure} = userSlice.actions; 
+    updateUserStart, updateUserSuccess, updateUserFailure,
+    deleteUserStart, deleteUserSuccess, deleteUserFailure} = userSlice.actions; 
 
 export default userSlice.reducer;
