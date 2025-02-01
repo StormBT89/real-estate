68c876adbc7a7b59c1216f4009257d1318bbe1d0
StormBT89 <glavevski.pece@gmail.com>
2025-02-01 09:00:56 +0100

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -71,4 +71,13 @@ export const google = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+export const signout = async (req, res, next) => {
+    try {
+        res.clearCookie('access_token');
+        res.status(200).json('User hss benn logged out');
+    } catch (error) {
+        next(error);
+    }
 }
