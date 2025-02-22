3da0c4d8759ed07032bdbb71f1dd0dc2746004bc
StormBT89 <glavevski.pece@gmail.com>
2025-02-22 15:48:37 +0100

    complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import { useState } from "react";
+import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/storage';
+import {app} from '../firebase';
 
 export default function CreateListing() {
+    const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageURLs: [],
+    });
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const [uploading, setUploading] = useState(false);
+    console.log(formData);
+
+    const handleImageSubmit = (e) => {
+        if (files.length > 0 && files.length + formData.imageURLs.length < 6) {
+            setUploading(true);
+            setImageUploadError(false);
+            const promises = [];
+
+            for (let i=0; i<files.length; i++) {
+                promises.push(storeImage(files[i]));
+            }
+            Promise.all(promises).then((urls) =>{
+                setFormData({...formData, imageURLs: formData.imageURLs.concat(urls),});
+                setImageUploadError(false);   
+                setUploading(false);             
+            }).catch((err) => {
+                setImageUploadError('Големината на фотографијата е потребно да биде до 2МБ');
+                setUploading(false);
+            });
+
+        } else {
+            setImageUploadError('Можете да додадете до 5 фотографии');
+            setUploading(false);
+        }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise ((resolve, reject) =>  {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                },
+                (error) => {
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+            );
+            
+        });
+    }
+    
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageURLs: formData.imageURLs.filter((_,i) => i !== index),
+        })
+    }
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'> 
@@ -72,15 +138,23 @@ export default function CreateListing() {
                     </span>
                     </p>        
                 <div className='flex gap-4'>
-                    <input className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple/>  
-                    <button className='p-3 text-green-700 border border-green-700 
-                                       rounded hover:shadow-lg disabled:opacity-80'>Прикачи</button>                  
+                    <input onChange={(e) => setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple/>  
+                    <button type="button" disabled={uploading} onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 
+                                       rounded hover:shadow-lg disabled:opacity-80'>Прикачи</button>  
+                    {uploading ? 'Се прикачува..' : 'Прикачи'}                
                 </div>
+                <p className="text-red-700">{imageUploadError && imageUploadError}</p>
+                {formData.imageURLs.length > 0 && formData.imageURLs.map((url, index) => (
+                    <div key={url} className="flex justify-between p-3 border items-center">
+                        <img src={url} alt="listing image" className="w-20 h-20 object-contain rounded-lg"></img>
+                        <button type="button" onClick={() => handleRemoveImage(index)} className="p-3 text-red-700 rounded-lg hover:opacity-75">Избриши</button>
+                    </div>
+                ))
+                }
                 <button className='p-3 bg-slate-700 text-white rounded-lg hover:opacity-95 disabled:opacity-80'>
-                    Креирајте ја недвожноста
+                    Креирајте ја недвижноста
                 </button>
-            </div>
-        
+            </div>    
         </form>
     </main>
   )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
-import { useRef, useState } from "react";
-import { set } from "mongoose";
+import { useRef, useState, useEffect } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase.js'; 
 import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure} from '../redux/user/userSlice.js';
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+
 
 export default function Profile() {
   const fileRef = useRef(null);
   const {currentUser, loading, error} = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
-  const [formData, setFormData] = useState({});
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});  
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
 
@@ -18,7 +22,36 @@ export default function Profile() {
   // allow read;
   // allow write;
   // request.resource.size < 2 * 1024 * 1024 &&
-  // request.resource.contentType.matches('image/.*)
+  // request.resource.contentType.matches('image/.*) 
+
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
+          setFormData({...formData, avatar: downloadURL}));
+      }
+    );  
+  };
+
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value});
@@ -87,8 +120,17 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center my-7'>Кориснички профил</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/>
-        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 
+        <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 
         object-cover cursor-pointer self-center mt-2"/>
+        <p className="text-sm self-center">
+          {fileUploadError ? 
+          (<span className="text-red-700">Error Image upload</span>
+
+          ) : filePerc > 0 && filePerc < 100 ? 
+          (<span className="text-slate-700">{`Uploading ${filePerc}%`}</span> )
+           : filePerc === 100 ? (<span className="text-green-700">Image successfully uploaded</span>)
+            : ("")}
+        </p>
         <input onChange={handleChange} type="text" id="username" placeholder="username" defaultValue={currentUser.username} className="border p-3 rounded-lg"/>
         <input onChange={handleChange} type="email" id="email" placeholder="email" defaultValue={currentUser.email} className="border p-3 rounded-lg"/>
         <input onChange={handleChange} type="password" id="password" placeholder="password" className="border p-3 rounded-lg"/> 
