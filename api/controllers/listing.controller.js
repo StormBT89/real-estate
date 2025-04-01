3714024efea7f9473388f0c8aae404aa4afebc75
StormBT89 <glavevski.pece@gmail.com>
2025-04-01 18:02:05 +0200

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -30,3 +30,29 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const updateListing = async (req, res, next) => {
+
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Недвижноста не е пронајдена'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'Може да ги ажурирате само недвижностите креирани од Ваша страна'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        );
+        
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+
+}
