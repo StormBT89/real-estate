0cc6dc096d0d8edbd3a19a418ceef3fef46384a0
StormBT89 <glavevski.pece@gmail.com>
2025-04-02 19:28:29 +0200

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,16 +2,21 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper'
+import { useSelector } from 'react-redux'
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle'
+import { FaBath, FaBed, FaChair, FaMapMarkedAlt, FaParking, FaShare } from 'react-icons/fa'
+
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-
+    const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
     const params = useParams();
+    const { currentUser } = useSelector((state) => state.user); 
 
     useEffect(() => {
 
@@ -56,11 +61,81 @@ export default function Listing() {
 
                     </SwiperSlide>))}
                 </Swiper>
+                <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+                    <FaShare 
+                        className='text-slate-500'
+                        onClick={() => {
+                            navigator.clipboard.writeText(window.location.href);
+                            setCopied(true);
+                            setTimeout(() => {
+                                setCopied(false);
+                            }, 2000);
+                        }}
+                    />
+                </div>
+                {copied && (
+                    <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+                        Линкот е копиран
+                    </p>
+                )}
+                <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+                    <p className='text-2xl font-semibold'>
+                        {listing.name} - ${' '}
+                        {listing.offer
+                        ? listing.discountPrice.toLocaleString('en-US')
+                        : listing.regularPrice.toLocaleString('en-US')
+                        }
+                        {listing.type === 'rent' && ' / month'}
+                    </p>
+                    <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+                        <FaMapMarkedAlt className='text-green-700'/>
+                        {listing.address}
+                    </p>
+                    <div className='flex gap-4'>
+                        <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                            {listing.type === 'rent' ? 'За изнајмување' : 'За продажба'}
+                        </p>
+                        {listing.offer && (
+                            <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                                ${+listing.regularPrice - +listing.discountPrice} попуст/намалување
+                            </p>
+                        )}
+                    </div>
+                    <p className='text-slate-800'>
+                        <span className='font-semibold text-black'>Карактеристики - </span>
+                        {listing.description}
+                    </p>
+                    <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                        <li className='flex items-center gap-1 whitespace-nowrap'>
+                            <FaBed className='text-lg'/>
+                            {listing.bedrooms > 1 
+                            ? `${listing.bedrooms} спални `
+                            : `${Listing.bedrooms} спална `}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap'>
+                            <FaBath className='text-lg'/>
+                            {listing.bathrooms > 1
+                                ? `${listing.bathrooms} бањи `
+                                : `${listing.bathrooms} бања `}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap'>
+                            <FaParking className='text-lg'/>
+                            {listing.parking ? 'Слободен паркинг простор' : 'Не е обезбеден паркинг простор '}
+                        </li>
+                        <li className='flex items-center gap-1 whitespace-nowrap'>
+                            <FaChair className='text-lg'/>
+                            {listing.furnished ? 'Обезбеден е мебел' : 'Не е обезбеден мебел'}
+                        </li>
+                    </ul>
+                    {currentUser && listing.userRef !== currentUser._id && !contact && (
+                        <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                            Контактирајте го сопственикот
+                        </button>
+                    )}  
+                </div>
             </div>
-        )
-        
+        )        
         }
-
     </main>
   )
 }
