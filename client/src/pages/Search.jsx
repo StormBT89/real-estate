1473dfbeccd8e60cdf1f6cebb580018f6da165c8
StormBT89 <glavevski.pece@gmail.com>
2025-04-06 14:30:21 +0200

    Create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
     const navigate = useNavigate();
@@ -15,8 +16,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
-    console.log(sidebardata);
+    
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -181,8 +181,21 @@ export default function Search() {
             <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95'>Пребарај</button>
         </form>
       </div>
-      <div>
+      <div className='flex-1'>
         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Резултати од пребарувањето</h1>
+        <div className='p-7 flex flex-wrap gap-4'>
+          {!loading && listings.length === 0 && (
+            <p className='text-xl text-slate-700'>Не е пронајдена недвижност согласно зададените параметри</p>
+          )}
+          {loading && (
+            <p className='text-xl text-slate-700 text-center w-full'>Се вчитуваат резултатите согласно пребарувањето ...</p>
+          )}
+          {
+            !loading && listings && listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))
+          }
+        </div>
       </div>
     </div>
   )
